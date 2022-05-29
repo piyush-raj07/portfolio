19ef74193ea8da5e9b4d2b1f0cdc07e2a78bca6c
unknown <piyush.12109255@lpu.in>
2022-05-29 10:21:48 +0530

    first commit

---
+++ portfolio.js
@@ -0,0 +1,30 @@
+function myfuntion(){
+    alert("Thank you for Contacting Us!");
+    document.getElementById('input').value='';
+    location.reload();
+  }
+
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("images");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+  } 
+  
